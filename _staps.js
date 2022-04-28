8675b16064e8d8d0683102e242e83492f7dc45f7
prokash <pprokash102@gmail.com>
2022-04-29 00:31:15 +0600

    mongodb connect and post api create

--- _staps.js
+++ _staps.js
@@ -1,6 +1,8 @@
 /**
+ * https://car-manager-server.herokuapp.com/
  * npm init
  * mpn i express mongodb cors dotenv
  * create index.js and setting package.json
  * require express,cors and create app and global get and listen
+ * create heroku server address and upload server
  */
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
-
+require("dotenv").config();
 // port setup
 const port = process.env.PORT || 5000;
 
@@ -10,6 +10,30 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// mongoDB
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.2685i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    await client.connect();
+    const carCollection = client.db("CarManager").collection("inventoryItems");
+    //post api
+    app.post("/cars", async (req, res) => {
+      const carInfo = req.body;
+      const cursor = await carCollection.insertOne(carInfo);
+      res.send(cursor);
+    });
+  } finally {
+    // await client.close();
+  }
+};
+run().catch(console.dir);
 //get global aip
 
 app.get("/", (req, res) => {
